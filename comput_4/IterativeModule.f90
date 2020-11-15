1286ad14ca54e62ffe2963b0fb123adea5e39189
JuSHeba <yulyashebalkova@gmail.com>
2020-11-16 00:27:16 +0300

    исправлена проблема с диагональным преобладанием

---
+++ comput_4/IterativeModule.f90
@@ -0,0 +1,116 @@
+module IterativeModule
+implicit none
+contains
+!_______________________________________________________________________
+! 1. Jacobi method
+!_______________________________________________________________________
+subroutine Jacobi(n, A, B, C, res,eps)
+
+  integer(8)  :: n, i, score, scoreMax
+  real(8), dimension(:), allocatable      :: B, res
+  real(8), dimension(1:size(B),1:size(B)) :: D, Z, invertD
+  real(8), dimension(:,:), allocatable    :: A, C
+  real(8), dimension(1:size(B))           :: G, res0
+  real(8) :: eps
+
+  write(*,*) 'You chose Jacobi method.'
+  write(*,*) 'Calculation solution vector..'
+
+  do i = 1,n
+    D(i,i) = C(i,i)
+    invertD(i,i) = 1/D(i,i)
+  end do
+
+  Z = matmul(invertD,(D - A))
+
+  do i = 1,n
+    G(i) = invertD(i,i)*B(i)
+  end do
+
+  score = 0
+  scoreMax = 100
+  res0 = res
+
+  do while (sqrt(sum((res0-res)**2))>eps .and. score<scoreMax)
+    res0 = res
+    res = matmul(Z,res) + G
+    score = score + 1
+  end do
+
+end subroutine Jacobi
+!_______________________________________________________________________
+! 2. Seidel method
+!_______________________________________________________________________
+subroutine Seidel(n, A, B, C, res, eps)
+
+  integer(8)  :: n, i, score, scoreMax
+  real(8), dimension(:,:), allocatable :: A, C
+  real(8), dimension(:), allocatable   :: res, B
+  real(8), dimension(1:size(B))        :: res0
+  real(8) :: eps
+
+  write(*,*) 'You chose Seidel method.'
+  write(*,*) 'Calculation solution vector..'
+
+  score    = 0
+  scoreMax = 100
+  res0 = res + 1d0
+
+  do while (sqrt(sum((res0-res)**2))>eps .and. score<scoreMax)
+    res0 = res
+    do i = 1,n
+        res(i) = (B(i) - dot_product(A(i,1:i-1),res(1:i-1)) &
+        - dot_product(A(i,i+1:n),res0(i+1:n))) / A(i,i)
+    end do
+    score = score + 1
+  end do
+
+end subroutine Seidel
+!_______________________________________________________________________
+! 3. Successive over-relaxation
+!_______________________________________________________________________
+subroutine SOR(n, A, B, C, res, eps)
+
+  integer(8)  :: n, i, j, score, scoreMax, max
+  real(8), dimension(:,:), allocatable :: A, C
+  real(8), dimension(:), allocatable   :: res, B
+    real(8), dimension(1:size(B),1:size(B)) :: P
+  real(8), dimension(1:size(B))        :: res0, Q, Q0, maxima
+  real(8) :: eps
+
+  write(*,*) 'You chose SOR method.'
+  write(*,*) 'Calculation solution vector..'
+
+  res = 0
+
+  P(i,j) = -A(i,j)/A(i,i)
+
+  Q(i) = B(i)/A(i,i)
+
+  Q0 = Q
+
+  score = 0
+  scoreMax = 100
+
+
+  do i = 1,n
+    maxima(i) = abs(Q(i))
+  end do
+  max = sum(maxloc(maxima))         ! мне очень стыдно за весь этот костыль
+
+
+  do while (abs(Q(max))>eps .and. score<scoreMax)
+    Q0 = Q
+    do concurrent(i=1:n)
+      Q(i) = Q0(i) + P(i,max)*Q0(max)
+    end do
+    score = score + 1
+  end do
+
+
+
+
+
+end subroutine SOR
+!_______________________________________________________________________
+end module IterativeModule
---
+++ comput_4/MethodIterative.f90
@@ -0,0 +1,100 @@
+program MethodIterative
+use IterativeModule
+implicit none
+!_______________________________________________________________________
+! DESCRIPTION OF THE PROGRAM :
+!_______________________________________________________________________
+! Variables used :
+!  n - matrix size
+!  A - system matrix
+!  B - column vector
+!  C - augmented system matrix
+!  R - residual
+!_______________________________________________________________________
+integer(8) :: n, i, j, bar
+real(8)    :: eps
+real(8), dimension(:,:), allocatable :: A, C
+real(8), dimension(:), allocatable   :: B, res, R
+
+character(len=72) :: key
+
+eps = 1e-4
+!_______________________________________________________________________
+! READING А & В
+!_______________________________________________________________________
+write(*,*) 'Readind data.dat..'
+open(12, file='data.dat')
+  read(12, '(2x, i10)')  n
+  allocate(A(n,n), B(n))
+
+  do i=1,n
+      read(12,*) A(i,1:n)
+  enddo
+
+  do i=1,n
+    read(12,*) B(i)
+  enddo
+close(12)
+!_______________________________________________________________________
+! GET THE AUGMENTED SYSTEM MATRIX
+!_______________________________________________________________________
+allocate(C(n,n+1))
+
+do i = 1,n
+  do j = 1,n
+    C(i,j) =  A(i,j)
+  end do
+end do
+
+do i=1,n
+  C(i,n+1) = B(i)
+end do
+
+allocate(res(n))
+!_______________________________________________________________________
+! TEST DIAGONALLY DOMINANT MATRIX
+!_______________________________________________________________________
+write(*,*) 'Testing diagonally dominant matrix..'
+
+do i = 1,n
+  bar = sum(abs(C(i,:)))
+  bar = bar - abs(C(i,i))
+  if(abs(C(i,i)) <= bar) then
+    write(*,*) 'This matrix does not have a diagonally dominant. &
+     Program exited.'
+    call exit()
+  end if
+end do
+
+write(*,*) 'This matrix has a diagonally dominant.'
+
+!_______________________________________________________________________
+! CALCULATION SOLUTION
+!_______________________________________________________________________
+call getarg(1, key)
+
+select case(key)
+case('jacobi')
+!    allocate(D(n,n), invertD(n,n), Z(n,n))
+!    allocate(G(n))
+    call Jacobi(n, A, B, C, res,eps)
+  case('seidel')
+    call Seidel(n, A, B, C, res, eps)
+  case('sor')
+    call SOR(n, A, B, C, res, eps)
+endselect
+!_______________________________________________________________________
+! DATA OUTPUT
+!_______________________________________________________________________
+open(22, file='result.dat')
+write(22,'("#", 1x, i0)') n
+do i=1,n
+ write(22,*) res(i)
+end do
+
+write(*,*) 'Result in result.dat'
+allocate(R(n))
+R = (sum((matmul(A, res) - B)**2))**0.5
+write(*,*) 'Residual R =', R
+
+end program MethodIterative
