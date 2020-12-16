db9c063c59fcc3630c4a60d8bd64365dc0b540c7
JuSHeba <yulyashebalkova@gmail.com>
2020-12-16 22:33:02 +0300

    версия 2

--- comput_7/fourier_module.f90
+++ comput_7/fourier_module.f90
@@ -2,33 +2,21 @@ module fourier_module
   implicit none
   contains
 
-    recursive subroutine FFT_DIF(x, key_sign)
-      integer :: key_sign, N, i
-      complex :: a
-      complex, dimension(:) :: x
-      complex, dimension(:), allocatable :: even, odd
-      real(8) :: pi
+    recursive subroutine FFT_DIF(x, y, w)
+      integer :: N, i
+      complex, dimension(0:) :: x, w
+      complex, dimension(0:size(x)-1) :: y
      !________________________________________________
-      pi = 4d0 * atan(1d0)
       N  = size(x)
 
-      if(N <= 1) return
-
-      allocate(even(N/2), odd((N+1)/2))
-      even = x(1:N:2)
-      odd  = x(2:N:2)
-
-      call FFT_DIF(even, key_sign)
-      call FFT_DIF(odd, key_sign)
-
-      do i = 1, N/2
-        a = exp(complex(0d0, key_sign*2d0*pi*i) / N) * even(i)
-        x(i)     = odd(i) + a
-        x(i+N/2) = odd(i) - a
-      end do
-
-      deallocate(even)
-      deallocate(odd)
+      if(N > 2) then
+        call FFT_DIF(x(0:N/2-1) + x(N/2:N-1),&
+                     y(0:N-2:2), w(0:N-1:2))
+        call FFT_DIF(w(0:N/2-1)*(x(0:N/2-1) - x(N/2:N-1)),&
+                     y(1:N-1:2), w(0:N-1:2))
+      else
+        forall (i = 0:1) y(i) = x(0) + (-1)**i*x(1)
+      end if
 
     end subroutine FFT_DIF
 
---
+++ comput_7/func_mod.f90
@@ -0,0 +1,22 @@
+module func_mod
+  implicit none
+  contains
+
+  !real(8) :: pi
+!  pi = 4d0 * atan(1d0)
+
+  pure function w_minus(q,p) result(w)
+    implicit none
+    integer, intent(in) :: q, p
+    complex :: w
+      w = exp(-2d0 * (4d0 * atan(1d0) * cmplx(0,1) * q/p))
+  end function w_minus
+
+  pure function w_plus(q,p) result(w)
+    implicit none
+    integer, intent(in) :: q, p
+    complex :: w
+      w = exp(2d0 * (4d0 * atan(1d0) * cmplx(0,1) * q/p))
+  end function w_plus
+
+end module func_mod
